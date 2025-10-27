6f248fa3dc1acf241b381787b5ba851fc36e87c8
unknown <marcopataracchia007@gmail.com>
2025-10-27 13:13:09 +0100

    moto1

---
+++ Prova1/Prova1/moto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova1
+{
+    internal class moto
+    {
+        public string Targa { get; set; }
+        private string Marca { get; set; }
+        private int Anno { get; set; }
+
+        public moto(string Targa, string Marca, int Anno)
+        {
+            this.Targa = Targa;
+            this.Marca = Marca;
+            this.Anno = Anno;
+        }
+
+        public virtual void MostraDettagli()
+        {
+            Console.WriteLine($"Veicolo:{Marca} {Targa}, Anno:{Anno}");
+        }
+    }
+}
